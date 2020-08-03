d8bf55384cac1e9daf934f11e378ae8f3e02aac9
Anas <anas.ansal10@gmail.com>
2020-08-03 07:21:02 +0530

    Updates

---
+++ precache-manifest.5e4b155dfcd9d7fe5f59069d424cb7df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995d8bcd61bbaeae94d7b5af614d56df",
+    "url": "/apollo-audio-assignment/index.html"
+  },
+  {
+    "revision": "dc47caab98f4079ba0e8",
+    "url": "/apollo-audio-assignment/static/css/2.9131a3d6.chunk.css"
+  },
+  {
+    "revision": "b5f5b310dd2675a1f6a8",
+    "url": "/apollo-audio-assignment/static/css/main.5f90a6d9.chunk.css"
+  },
+  {
+    "revision": "dc47caab98f4079ba0e8",
+    "url": "/apollo-audio-assignment/static/js/2.4d330f83.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/apollo-audio-assignment/static/js/2.4d330f83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f5b310dd2675a1f6a8",
+    "url": "/apollo-audio-assignment/static/js/main.3485b385.chunk.js"
+  },
+  {
+    "revision": "c02271150826ce59693f",
+    "url": "/apollo-audio-assignment/static/js/runtime-main.909af57b.js"
+  }
+]);
